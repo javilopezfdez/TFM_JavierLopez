4664b930eaccdbfc00e837d760f8b0864534560a
javilopezfdez <javierlopezfdezz@gmail.com>
2026-05-14 11:11:46 +0200

    Add files via upload

---
+++ 00_utils.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 13 12:21:56 2026
+
+@author: Javier López Fernández
+Contacto: javier.lopez.fernandez@alumnos.uvigo.es
+
+Última modificación: 14/05/2026
+"""
+
+import pandas as pd #Lectura de archivos EXCEL
+import matplotlib.pyplot as plt #Graficar resultados
---
+++ 01_load.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 13 12:21:56 2026
+
+@author: Javier López Fernández
+Contacto: javier.lopez.fernandez@alumnos.uvigo.es
+
+Última modificación: 14/05/2026
+"""
+file_path = r"C:\Users\javil\Desktop\TFM\excel\SanSimon_6_decimal.xlsx"
+
