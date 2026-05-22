b11149f771f4468747f3caec83261ef7ec377834
Javier López <javierlopezfdezz@gmail.com>
2026-05-22 12:10:23 +0200

    Update and rename 14_diferencias_local_producto_FCO2.py to 15_diferencias_local_producto_FCO2.py
    Diferencias entre flujos de modelos y estación.

--- 15_diferencias_local_producto_FCO2.py
+++ 15_diferencias_local_producto_FCO2.py
@@ -5,13 +5,13 @@ Created on Wed May 20 16:44:38 2026
 @author: Javier López Fernández
 Contacto: javier.lopez.fernandez@alumnos.uvigo.es
 
-Última modificación:
+Última modificación:21/05/2026
 """
 
 import pandas as pd
 import matplotlib.pyplot as plt
 
-archivo = r"C:\Users\javil\Desktop\TFM\excel\SS7_CON_VIENTOS.xlsx"
+archivo = r"C:\Users\javil\Desktop\TFM\excel\MUY_AVANZADOS\SS7_FLUJOS_BAT_VIENTOS.xlsx"
 
 df = pd.read_excel(archivo)
 
@@ -22,18 +22,24 @@ df["hora_hh_mm_ss"] = pd.to_datetime(
 )
 
 df["diferencia"] = df["FCO2_molCm2yr"] - df["FCO2_cop"]
+df["diferencia_1"] = df["FCO2_molCm2yr"] - df["FCO2_viso"]
 
 fig, ax = plt.subplots(2, 1, figsize=(10, 7), sharex=True)
 
 ax[0].plot(
     df["hora_hh_mm_ss"],
     df["FCO2_molCm2yr"],
-    label="FCO2_METEOGALICIA"
+    label="FCO2_METEOGALICIA_MOD"
 )
 ax[0].plot(
     df["hora_hh_mm_ss"],
     df["FCO2_cop"],
     label="FCO2_COPERNICUS"
+)
+ax[0].plot(
+    df["hora_hh_mm_ss"],
+    df["FCO2_viso"],
+    label="FCO2_OViso"
 )
 ax[0].set_ylabel("FCO2")
 ax[0].set_title("FCO2 vs tiempo")
@@ -44,8 +50,15 @@ ax[1].plot(
     df["hora_hh_mm_ss"],
     df["diferencia"],
     color="black",
-    label="Diferencia METEOGALICIA - COPERNICUS"
+    label="Diferencia METEOGALICIA_MOD - COPERNICUS"
 )
+ax[1].plot(
+    df["hora_hh_mm_ss"],
+    df["diferencia_1"],
+    color="blue",
+    label="Diferencia METEOGALICIA_MOD - O Viso"
+)
+
 ax[1].axhline(0, color="gray", linestyle="--", linewidth=1)
 ax[1].set_xlabel("Tiempo")
 ax[1].set_ylabel("Diferencia FCO2")
