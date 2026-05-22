fb420af0ca1bda6ae0ec62dc38808a7d9737bd30
Javier López <javierlopezfdezz@gmail.com>
2026-05-22 12:09:04 +0200

    16_diferencias_vientos.py
    Diferencias entre los vientos de los modelos meteogalicia y Copernicus y de la estación O Viso.

--- 16_diferencias_vientos.py
+++ 16_diferencias_vientos.py
@@ -5,13 +5,13 @@ Created on Wed May 20 13:07:01 2026
 @author: Javier López Fernández
 Contacto: javier.lopez.fernandez@alumnos.uvigo.es
 
-Última modificación:
+Última modificación: 21/05/2026
 """
 
 import pandas as pd
 import matplotlib.pyplot as plt
 
-archivo = r"C:\Users\javil\Desktop\TFM\excel\SS7_CON_VIENTOS.xlsx"
+archivo = r"C:\Users\javil\Desktop\TFM\excel\MUY_AVANZADOS\SS7_FLUJOS_BAT_VIENTOS.xlsx"
 
 df = pd.read_excel(archivo)
 
@@ -22,18 +22,24 @@ df["hora_hh_mm_ss"] = pd.to_datetime(
 )
 
 df["diferencia"] = df["ws"] - df["ws_cop"]
+df["diferencia_1"] = df["ws"] - df["viento_m_s"]
 
 fig, ax = plt.subplots(2, 1, figsize=(10, 7), sharex=True)
 
 ax[0].plot(
     df["hora_hh_mm_ss"],
     df["ws"],
-    label="Viento_METEOGALICIA"
+    label="Viento_METEOGALICIA_MOD"
 )
 ax[0].plot(
     df["hora_hh_mm_ss"],
     df["ws_cop"],
     label="Viento_COPERNICUS"
+)
+ax[0].plot(
+    df["hora_hh_mm_ss"],
+    df["viento_m_s"],
+    label="Viento_OViso"
 )
 ax[0].set_ylabel("Viento")
 ax[0].set_title("Viento vs tiempo")
@@ -44,7 +50,13 @@ ax[1].plot(
     df["hora_hh_mm_ss"],
     df["diferencia"],
     color="black",
-    label="Diferencia METEOGALICIA - COPERNICUS"
+    label="Diferencia METEOGALICIA_MOD - COPERNICUS"
+)
+ax[1].plot(
+    df["hora_hh_mm_ss"],
+    df["diferencia_1"],
+    color="blue",
+    label="Diferencia METEOGALICIA_MOD - O Viso"
 )
 ax[1].axhline(0, color="gray", linestyle="--", linewidth=1)
 ax[1].set_xlabel("Tiempo")
