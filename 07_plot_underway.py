9adf48f0f3384d4ca6bd849f8edbdccd5a81f8ae
Javier López <javierlopezfdezz@gmail.com>
2026-05-27 09:57:27 +0200

    Cambios en labels

--- 07_plot_underway.py
+++ 07_plot_underway.py
@@ -5,7 +5,7 @@ Created on Wed May 13 12:20:38 2026
 @author: Javier López Fernández
 Contacto: javier.lopez.fernandez@alumnos.uvigo.es
 
-Última modificación: 19/05/2026
+Última modificación: 27/05/2026
 """
 #MF comment: aunque le hayas llamado 07, ejecútalo al final del todo, cuando ya tengas los valores ajustados/calibrados, etc.
 
@@ -44,7 +44,7 @@ axs[0].plot(
     df["hora_hh_mm_ss"],
     df["pCO2"],
     color="blue",
-    label="pCO2 crudo"
+    label="Raw pCO2\n"
 )
 
 axs[0].plot(
@@ -52,10 +52,10 @@ axs[0].plot(
     df["pCO2_ajustada"],
     color="red",
     linestyle="--",
-    label="pCO2 corregido"
+    label="Corrected pCO2\n"
 )
 
-axs[0].set_ylabel("pCO2 (µatm)")
+axs[0].set_ylabel("pCO2\n (µatm)")
 axs[0].set_ylim(320,500)
 axs[0].legend()
 axs[0].grid(True)
@@ -69,7 +69,7 @@ axs[1].plot(
     color="green"
 )
 
-axs[1].set_ylabel("O2")
+axs[1].set_ylabel("Oxygen (mg/L)")
 axs[1].grid(True)
 
 
@@ -81,7 +81,7 @@ axs[2].plot(
     color="orange"
 )
 
-axs[2].set_ylabel("Temp (°C)")
+axs[2].set_ylabel("Temperature (°C)")
 axs[2].grid(True)
 
 
@@ -105,7 +105,7 @@ axs[4].plot(
     color="purple"
 )
 
-axs[4].set_ylabel("Salinity") #MF comment: pasa todas las labels a inglés e incluye unidades.
+axs[4].set_ylabel("Salinity (PSU)") #MF comment: pasa todas las labels a inglés e incluye unidades.
 axs[4].grid(True)
 
 
@@ -118,14 +118,14 @@ axs[5].plot(
     linewidth=1
 )
 
-axs[5].set_ylabel("Clorofila") #MF comment: pasa todas las labels a inglés e incluye unidades.
-axs[5].set_ylim(0,0.2) #MF: rango demasiado restrictivo
-axs[5].set_xlabel("Hora")
+axs[5].set_ylabel("Chlorophyll (mg/L)") #MF comment: pasa todas las labels a inglés e incluye unidades.
+#axs[5].set_ylim(0,0.2) #MF: rango demasiado restrictivo
+axs[5].set_xlabel("Time")
 axs[5].grid(True)
 
 #------------------------------------------------------------------------
 
-plt.suptitle("Serie temporal de las distintas variables.", fontsize=14)
+plt.suptitle("Multiple variables time series.", fontsize=14)
 
 plt.tight_layout()
 plt.show()
